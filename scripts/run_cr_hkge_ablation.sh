31555e7bc783b8c812f42e4d537dcad85f72d05b
azamazri <azamazrri@gmail.com>
2026-06-04 01:41:55 +0700

    Add final CR-HKGE alpha variants

--- scripts/run_cr_hkge_ablation.sh
+++ scripts/run_cr_hkge_ablation.sh
@@ -46,9 +46,18 @@ variant_args() {
     FINAL_strict_gated)
       echo "--cr_use_relation_weight 1 --cr_use_cross_ref 1 --cr_relation_weight_mode semantic --cr_relation_aware_message 0 --cr_relation_message_scale type_count --cr_cross_ref_bi_interaction 0 --cr_cross_ref_gate 1 --cr_cross_ref_gate_init -2.0"
       ;;
+    FINAL_strict_additive)
+      echo "--cr_use_relation_weight 1 --cr_use_cross_ref 1 --cr_relation_weight_mode semantic --cr_relation_aware_message 0 --cr_relation_message_scale type_count --cr_cross_ref_bi_interaction 0 --cr_cross_ref_gate 0 --cr_cross_ref_alpha 1.0"
+      ;;
+    FINAL_alpha_0_5)
+      echo "--cr_use_relation_weight 1 --cr_use_cross_ref 1 --cr_relation_weight_mode semantic --cr_relation_aware_message 0 --cr_relation_message_scale type_count --cr_cross_ref_bi_interaction 0 --cr_cross_ref_gate 0 --cr_cross_ref_alpha 0.5"
+      ;;
+    FINAL_alpha_1_5)
+      echo "--cr_use_relation_weight 1 --cr_use_cross_ref 1 --cr_relation_weight_mode semantic --cr_relation_aware_message 0 --cr_relation_message_scale type_count --cr_cross_ref_bi_interaction 0 --cr_cross_ref_gate 0 --cr_cross_ref_alpha 1.5"
+      ;;
     *)
       echo "unknown variant: $1" >&2
-      echo "available: A1_no_crossref A2_no_relation_message A3_probability_scale A4_no_novelty_modules FINAL_strict_gated" >&2
+      echo "available: A1_no_crossref A2_no_relation_message A3_probability_scale A4_no_novelty_modules FINAL_strict_gated FINAL_strict_additive FINAL_alpha_0_5 FINAL_alpha_1_5" >&2
       return 1
       ;;
   esac
